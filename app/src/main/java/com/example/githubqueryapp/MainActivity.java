5251cb1a8ef6e78b37b0a3f51ed5f3ef647de284
swapan <swapan.d83@gmail.com>
2020-05-21 21:00:03 +0600

    Add Menu

--- app/src/main/java/com/example/githubqueryapp/MainActivity.java
+++ app/src/main/java/com/example/githubqueryapp/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.githubqueryapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -32,6 +37,22 @@ public class MainActivity extends AppCompatActivity {
         // C Use findViewById to get a reference to mSearchResultsTextView
         mSearchResultsTextView = (TextView) findViewById(R.id.tv_github_search_results_json);
 
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int itemThatWasClickedId = item.getItemId();
+        if (itemThatWasClickedId == R.id.action_search){
+            Context context = MainActivity.this;
+            String textToShow = "Search Clicked";
+            Toast.makeText(context,textToShow,Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
